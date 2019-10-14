65a420d9f91dcfc8882f7371e9fd215492d9c7c4
Alan Dragomirecký <alan@dragomirecky.com>
2019-10-14 15:59:01 +0200

    Update Package.swift with new repository name

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "UnicodeSupport",
+    name: "unicode-support",
     products: [
         .library(name: "SimpleUnicodeSupport", targets: ["SimpleUnicodeSupport"]),
     ],
